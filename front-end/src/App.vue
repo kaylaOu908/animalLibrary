605a83d36d378de8d0652ef7ff5eac637afb5b27
Kayla Ou <kayla.ou99@gmail.com>
2022-04-10 00:55:03 -0600

    initial commit

---
+++ front-end/src/App.vue
@@ -0,0 +1,79 @@
+<template>
+<div id="app">
+  <div class="header">
+    <router-link to="/">
+      <div class="logo">
+        <img src="/logo.png">
+      </div>
+    </router-link>
+  </div>
+  <div class="content">
+    <router-view />
+  </div>
+  <div class="footer">
+    <router-link to="/admin">Admin</router-link><br>
+    <a href="https://github.com/kaylaOu908/animalLibrary.git">GitHub</a>
+  </div>
+</div>
+</template>
+
+<style>
+html {
+  box-sizing: border-box;
+}
+
+body {
+  font-family: 'Montserrat', sans-serif;
+  font-size: 16px;
+  background: #fff;
+  padding: 0px;
+  margin: 0px;
+}
+
+/* Header */
+.header {
+  display: flex;
+  padding: 0px 85px 0px 85px;
+  background-color: #e3e3e3;
+  color: #e3e3e3;
+}
+
+.title {
+  margin-top: 5px;
+}
+
+.title h1 {
+  font-size: 30px;
+}
+
+.content {
+  padding: 20px 100px;
+  min-height: 500px;
+}
+.logo{
+  margin-left: auto;
+  margin-right: auto;
+  display: block;
+}
+/* Footer */
+.footer {
+  height: 50px;
+  padding: 20px 100px 0px 100px;
+  background: #e3e3e3;
+  font-size: 12px;
+  text-align: center;
+
+}
+
+.footer a {
+  color: #000;
+}
+
+h1 {
+  font-size: 20px;
+}
+
+h2 {
+  font-size: 14px;
+}
+</style>
